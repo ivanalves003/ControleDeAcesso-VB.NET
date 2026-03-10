d70dcbd2aaa424a21200d85ae8fc17d15897c3f8
Ivan Alves <ivanalves2508@gmail.com>
2026-03-10 17:17:58 -0300

    Add new menu item handlers for person actions

--- FrmControleDeAcessoMDI.vb
+++ FrmControleDeAcessoMDI.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Windows.Forms
+Imports System.Windows.Forms
 Imports GGeral
 Imports TratamentoDeErros
 Imports xAlimento
@@ -6,17 +6,61 @@ Imports xAlimento
 Public Class FrmControleDeAcessoMDI
 
     Private Sub MnuTipo_Click_1(sender As Object, e As EventArgs) Handles MnuTipo.Click
-        Try
+1:      Try
 
-            CfgForm.AbrirFormulario(FrmTipo.Instance, Me, False, 0)
+2:          CfgForm.AbrirFormulario(FrmTipo.Instance, Me, False, 0)
 
-        Catch ex As Exception
+3:      Catch ex As Exception
 
-            Windows.Forms.Cursor.Current = Cursors.Default
-            Erro.Log(ConexaoSQL.VersaoAplicacao, System.Reflection.Assembly.GetExecutingAssembly.GetName.Name(), ex.Message & " " & ex.StackTrace, Me.Name, System.Reflection.MethodBase.GetCurrentMethod().Name, True, CStr(Erl()))
+4:          Windows.Forms.Cursor.Current = Cursors.Default
+5:          Erro.Log(ConexaoSQL.VersaoAplicacao, System.Reflection.Assembly.GetExecutingAssembly.GetName.Name(), ex.Message & " " & ex.StackTrace, Me.Name, System.Reflection.MethodBase.GetCurrentMethod().Name, True, CStr(Erl()))
 
-        Finally
-        End Try
+6:      Finally
+7:      End Try
+
+    End Sub
+
+    Private Sub MnuPessoaSeleciona_Click(sender As Object, e As EventArgs) Handles MnuPessoaSeleciona.Click
+
+8:      Try
+
+9:          CfgForm.AbrirFormulario(FrmSelecionarPessoa.Instance, Me, False, 0)
+
+10:     Catch ex As Exception
+
+11:         Windows.Forms.Cursor.Current = Cursors.Default
+12:         Erro.Log(ConexaoSQL.VersaoAplicacao, System.Reflection.Assembly.GetExecutingAssembly.GetName.Name(), ex.Message & " " & ex.StackTrace, Me.Name, System.Reflection.MethodBase.GetCurrentMethod().Name, True, CStr(Erl()))
+
+13:     Finally
+14:     End Try
+
+    End Sub
+
+    Private Sub MnuPessoaIncluir_Click(sender As Object, e As EventArgs) Handles MnuPessoaIncluir.Click
+
+15:     Try
+
+16:         CfgForm.AbrirFormulario(FrmIncluirPessoa.Instance, Me, False, 0)
+
+17:     Catch ex As Exception
+
+18:         Windows.Forms.Cursor.Current = Cursors.Default
+19:         Erro.Log(ConexaoSQL.VersaoAplicacao, System.Reflection.Assembly.GetExecutingAssembly.GetName.Name(), ex.Message & " " & ex.StackTrace, Me.Name, System.Reflection.MethodBase.GetCurrentMethod().Name, True, CStr(Erl()))
+
+20:     Finally
+21:     End Try
+
+    End Sub
+
+    Private Sub SelecionarToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SelecionarToolStripMenuItem1.Click
+
+22:     Try
+
+23:         CfgForm.AbrirFormulario(FrmLiberacao.Instance, Me, False, 0)
+
+24:     Catch ex As Exception
+25:         Erro.Tratar(ex, Erl.ToString)
+26:     End Try
 
     End Sub
 End Class
